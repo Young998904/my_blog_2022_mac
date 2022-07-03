7037ed6ddcee16bbd325c931bdd891262dc3d1fb
Young998904 <like0998904@gmail.com>
2022-07-03 17:45:10 +0900

    사이트 이름 꾸며서 표시

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
-    description: "조아영이 Mac 에서 만든 gatsby blog.",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
