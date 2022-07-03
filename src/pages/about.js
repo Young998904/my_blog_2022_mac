28488ea5484b0a65e353693429fe18bb30283bc6
Young998904 <like0998904@gmail.com>
2022-07-03 17:02:24 +0900

    about 페이지 추가

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>AboutPage</title>
+      <h1>자기소개 페이지 입니다</h1>
+    </main>
+  );
+};
+
+export default AboutPage;
